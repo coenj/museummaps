1cd554d5c7cd6c130892123d067fe6ddbe31d083
Coen <coenc2000@hotmail.com>
2017-07-19 10:26:27 +0200

    write to document in wiki_views instead maps

---
+++ js/museummaps.js
@@ -0,0 +1,86 @@
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+var mContent;
+var map;
+var infowindow;
+var service;
+function initMap() {
+  var searchcity = { lat: 51.908082, lng: 4.480408 };
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: searchcity,
+    zoom: 13
+  });
+  var geocoder = new google.maps.Geocoder();
+
+  document.getElementById('submit').addEventListener('click', function () {
+    geocodeAddress(geocoder, map);
+  });
+
+  function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('address').value;
+    mContent = "";
+    geocoder.geocode({ 'address': address }, function (results, status) {
+      if (status === 'OK') {
+        searchcity = results[0].geometry.location;
+        resultsMap.setCenter(searchcity);
+        var marker = new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location
+        });
+        // search all nearby art museums
+        infowindow = new google.maps.InfoWindow();
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: searchcity,
+          radius: 3000,
+          type: ['museum'],
+          keyword: 'art'
+        }, callback);
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  document.getElementById('museum-review').innerHTML = "";
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: placeLoc
+  });
+
+
+  function getContent() {
+    var markerContent = place.geometry.location + place.name + place.opening_hours;
+    var placeNameWiki= place.name.replace(/ /g, "_")
+/*
+    if (place.photos != undefined) {
+       var photoUrl = place.photos[0].getUrl({maxWidth: 400, maxHeight: 400});
+      markerContent ="<img src="+ photoUrl +">"
+      console.log(markerContent)
+    }
+*/
+    
+    wikiViews(place.name, placeNameWiki)
+
+    google.maps.event.addListener(marker, 'click', function () {
+      infowindow.setContent(markerContent);
+      infowindow.open(map, marker);
+    });
+  }
+
+  getContent();
+}
--- js/wiki_views.js
+++ js/wiki_views.js
@@ -3,33 +3,38 @@ function shortDate(passedDate) {
     return passedDate
 }
 
-function wikiViews(searchpage) {
-searchpage=searchpage.replace(/ /g,"_")
-   
-        var date = new Date(Date.now())
-        var endDate = shortDate(date.toISOString())
+function wikiViews(pageName, searchpage) {
+    searchpage = searchpage.replace(/The /g, "")
+    searchpage = searchpage.replace(/Le /g, "")
 
-        // change date to 1 month before on 1st day
-        console.log(date)
-        date.setMonth(date.getMonth() - 1);
-        date.setDate(1);
-        var startDate = shortDate(date.toISOString())
+    var date = new Date(Date.now())
+    var endDate = shortDate(date.toISOString())
 
-        console.log(startDate)
-        var url = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia.org/all-access/all-agents/" + searchpage + "/monthly/" + startDate + "/" + endDate
+    mContent += '<div class=place id=' + searchpage + '><div class=place-name><a href=https://en.wikipedia.org/wiki/' + searchpage + '>' + pageName + '"</a></div></div>'
 
-        var views = $.ajax({
-            url: url,
-            dataType: 'json',
-            success: function (data) {
-                response = data;
-            }
-        }).done(function (response) {
-            console.log(response);
-            console.log(response.items[0].views); // <<< number of views of this page!
-            document.getElementById(searchpage).innerHTML+=" wiki views:<b>" + response.items[0].views+"</b><br>"
-            var result = response.items[0].views
-            return result
 
-        })
-    }
+    document.getElementById('museum-review').innerHTML += mContent;
+
+    // change date to 1 month before on 1st day
+    console.log(date)
+    date.setMonth(date.getMonth() - 1);
+    date.setDate(1);
+    var startDate = shortDate(date.toISOString())
+
+    console.log(startDate)
+    var url = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia.org/all-access/all-agents/" + searchpage + "/monthly/" + startDate + "/" + endDate
+    // get the number of views the page has on wikipedia
+    var views = $.ajax({
+        url: url,
+        dataType: 'json',
+        success: function (data) {
+            response = data;
+        }
+    }).done(function (response) {
+        document.getElementById(searchpage).innerHTML += '<div class=place-viewbox style="background-color: rgb(0,25,' + Math.floor(response.items[0].views / 100) + ')"><div class=place-views>' + Math.floor(response.items[0].views / 100) + '</div></div>'
+
+        var result = response.items[0].views
+        return result
+
+    })
+}
